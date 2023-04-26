adf601826adef39200ab8419246186797122927f
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-04-26 13:37:05 -0400

    Testing Water Particles

--- TestUnity_Example/Assets/MyAssets/MyScripts/Destroy.cs
+++ TestUnity_Example/Assets/MyAssets/MyScripts/Destroy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    public float destroySpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Destroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Destroy(gameObject, 2.0f);
+        Destroy(gameObject, destroySpeed);
     }
 }
