8d055344411cf0f23059b69ec8f4ec7b1188590e
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-05-02 19:54:46 -0400

    Testing with emission lerping

--- TestUnity_Example/Assets/MyAssets/MyScripts/Emissive_Flicker.cs
+++ TestUnity_Example/Assets/MyAssets/MyScripts/Emissive_Flicker.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Emissive_Flicker : MonoBehaviour
 {
+    public GameObject assetOBJ;
+    public Material emissiveMTL;
+    public Color BaseColor;
+
+    public AnimationCurve emissiveCurve;
+
+    public float startingValue;
+    public float secondValue;
+    public float emissionSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        emissiveMTL.SetColor("_EmissionColor", BaseColor * startingValue);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startingValue < secondValue)
+            firstFlicker();
+    }
+
+    void firstFlicker()
+    {
+        startingValue = Mathf.MoveTowards(startingValue, secondValue, emissiveCurve.Evaluate(emissionSpeed * Time.deltaTime));
+        emissiveMTL.SetColor("_EmissionColor", BaseColor * startingValue);
     }
 }
