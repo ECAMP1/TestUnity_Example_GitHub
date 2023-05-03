34a3a7b3e852628332686500042e45f6f52bf76c
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-05-03 14:12:38 -0400

    Emissive_Light Intensity Lerp

--- TestUnity_Example/Assets/MyAssets/MyScripts/Emissive_Flicker.cs
+++ TestUnity_Example/Assets/MyAssets/MyScripts/Emissive_Flicker.cs
@@ -10,7 +10,6 @@ public class Emissive_Flicker : MonoBehaviour
     public AnimationCurve curve;
 
     public float emissionMax;
-    //public float curveSpeed;
 
     
     // Start is called before the first frame update
@@ -22,9 +21,7 @@ public class Emissive_Flicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //emissiveColor = Color.Lerp(Color.yellow, Color.black, Mathf.PingPong(Time.time, emissionSpeed));
-
-        emissiveColor = Color.Lerp(Color.yellow, Color.black, Mathf.PingPong(curve.Evaluate (Time.time), emissionMax));
+        emissiveColor = Color.Lerp(Color.black, Color.yellow, Mathf.PingPong(curve.Evaluate (Time.time), emissionMax));
         emissiveMTL.SetColor("_EmissionColor", emissiveColor);
     }
 }
