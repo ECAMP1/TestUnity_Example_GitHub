cbf0a999f9e18c3b2b656114bda7162f543cdfdb
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-05-23 16:03:52 -0400

    Updated Lerp

---
+++ TestUnity_Example/Assets/MyAssets/MyScripts/Door_RotationLerp_OnCommand.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_RotationLerp_OnCommand : MonoBehaviour
+{
+    public GameObject door;
+    public KeyCode keyCommand;
+    public Vector3 rotation_Origin;
+    public Vector3 rotation_Goal;
+    public float speed;
+
+    private bool _isReady;
+    private bool _isUsing;
+    private bool _isOpened;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isReady = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isReady)
+            DoorCTRL();
+
+        if (_isOpened)
+            door.transform.rotation = Quaternion.Lerp(Quaternion.Euler(door.transform.position), Quaternion.Euler(rotation_Goal), speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isReady = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isReady = false;
+    }
+
+    void DoorCTRL()
+    {
+        if (Input.GetKeyDown(keyCommand))
+            _isUsing = !_isUsing;
+        if (_isUsing)
+        {
+            _isOpened = true;
+            Debug.Log("using");
+        }
+        else
+            _isOpened = false;
+    }
+}
--- TestUnity_Example/Assets/MyAssets/MyScripts/Elevator_Lerp.cs
+++ TestUnity_Example/Assets/MyAssets/MyScripts/Elevator_Lerp.cs
@@ -1,39 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Elevator_Lerp : MonoBehaviour
 {
     public GameObject elevator_OBJ;
-    public Vector3 position_01;
-    public Vector3 position_02;
-    public float speed;
+    public Vector3 position_Origin;
+    public Vector3 position_Goal;
+    public AnimationCurve curve;
     public KeyCode keyCommand;
-
-    private Transform origin;
+    public float speed;
+   
+    private GameObject _thePlayer;
     private bool _isSTILL;
     private bool _isGoing;
+    private bool _isIn;
+
     // Start is called before the first frame update
     void Start()
     {
+        _thePlayer = GameObject.FindWithTag("Player");
         _isGoing = false;
+        _isIn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_isGoing)
+        if (_isIn)
             elevatorControl();
+
+        if (_isGoing)
+            elevator_OBJ.transform.position = Vector3.Lerp(elevator_OBJ.transform.position, position_Goal, curve.Evaluate(speed * Time.deltaTime));
+        else
+            elevator_OBJ.transform.position = Vector3.Lerp(elevator_OBJ.transform.position, position_Origin, curve.Evaluate (speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            _thePlayer.transform.parent = elevator_OBJ.transform;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        _isGoing = true;
+        _isIn = true;
     }
-
     private void OnTriggerExit(Collider other)
     {
-        _isGoing = false;
+        _isIn = false;
+        if (other.tag == "Player")
+            _thePlayer.transform.parent = null;
     }
 
     void elevatorControl()
@@ -42,9 +60,15 @@ public class Elevator_Lerp : MonoBehaviour
         {
             _isSTILL = !_isSTILL;
             if (_isSTILL)
-                elevator_OBJ.transform.position = Vector3.Lerp(elevator_OBJ.transform.localPosition, position_02, speed * Time.deltaTime);
+            {
+                _isGoing = true;
+                Debug.Log("1st check");
+            }
             else
-                elevator_OBJ.transform.position = Vector3.Lerp(position_02 ,position_01, speed * Time.deltaTime);
+            {
+                _isGoing = false;
+                Debug.Log("2nd check");
+            }
 
         }
     }
